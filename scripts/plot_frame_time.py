d9524c9bc7322aca852e583d73b2601d109cd090
Daniel Lo <dl575@cornell.edu>
2013-04-22 15:09:40 -0400

    Plot scripts sepeartes out slow and fast execution times.

--- scripts/plot_frame_time.py
+++ scripts/plot_frame_time.py
@@ -14,18 +14,27 @@ if len(sys.argv) != 2:
 # Open file
 f = open(sys.argv[1], 'r')
 
-times = []
+# Separately save times that are above or below the threshold
+threshold = 10000
+fast_times = {}
+slow_times = {}
 # For each line
 for line in f:
   # Use regular expressions to parse
-  r = re.search("(\d+)us", line)
+  r = re.search("Frame (\d+) = (\d+)us", line)
   if r:
-    times.append(r.group(1))
+    frame = int(r.group(1))
+    time = int(r.group(2))
+    if time > threshold:
+      slow_times[frame] = time
+    else:
+      fast_times[frame] = time
 
 # Plot
 fig = plot.figure()
 ax1 = plot.subplot(111)
-ax1.plot(times)
+ax1.scatter(fast_times.keys(), fast_times.values(), c='b')
+ax1.scatter(slow_times.keys(), slow_times.values(), c='r')
 ax1.set_xlabel("Frame")
 ax1.set_ylabel("Execution time [us]")
 plot.show()
