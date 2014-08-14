3811caff0b546d9be1d2f00482f39c497955dad5
Daniel Lo <dl575@cornell.edu>
2014-08-14 14:21:04 -0400

    Optimize slicing script.
    Don't redo slicing for already handled variables.
    Re-use existing Visitors instead of declaring new ones.

--- pycparser/examples/slice.py
+++ pycparser/examples/slice.py
@@ -30,6 +30,14 @@ class IDVisitor(c_ast.NodeVisitor):
   def __init__(self):
     self.IDs = []
     self.cgenerator = c_generator.CGenerator()
+  def reset(self):
+    self.IDs = []
+  """
+  Reset list of IDs, then visit passed node.
+  """
+  def new_visit(self, node):
+    self.reset()
+    self.visit(node)
   def visit_ID(self, node):
     if node.name not in self.IDs:
       self.IDs.append(node.name)
@@ -42,25 +50,45 @@ Identify all data dependencies of the passed variable var.
 """
 class DataDependencyVisitor(c_ast.NodeVisitor):
   def __init__(self, var):
+    # Variable of interest
     self.var = var
-    self.rvalues = set()
-    self.declvalues = set() # Variables included in slice, ensure declared
-    self.id_visitor = IDVisitor()
-    self.cgenerator = c_generator.CGenerator()
+    # Set of rvalues to be used for future DataDependenceVisitors
+    self.rvalues = []
     # Label to jump to on return statements
     self.end_label = "print_loop_counter"
-  # Mark node as sliced and add rvalues to saved set
+
+    # Helper visitors
+    self.id_visitor = IDVisitor()
+    self.cgenerator = c_generator.CGenerator()
+  """
+  Reinitialize DataDependencyVisitor with a new target variable.
+  """
+  def reset(self, var):
+    self.var = var
+    self.rvalues = []
+  """
+  Reinitialize and run a new visit.
+  """
+  def new_visit(self, var, node):
+    self.reset(var)
+    self.visit(node)
+
+  """
+  Mark node as sliced and add rvalues to saved set
+  """
   def slice_data(self, node):
     node.sliced = True
-    self.id_visitor.visit(node)
-    self.rvalues = self.rvalues.union(set(self.id_visitor.IDs))
+    self.id_visitor.new_visit(node)
+    self.rvalues += self.id_visitor.IDs
     """
     print "Adding to slice:"
     print_node(node)
     print "because of data dependence %s" % self.var
     print
     """
-  # Get base name of array without index
+  """
+  Get base name of array without index
+  """
   def get_ArrayRef_name(self, node):
     if isinstance(node, c_ast.ID):
       return node.name
@@ -117,9 +145,10 @@ class DataDependencyVisitor(c_ast.NodeVisitor):
         node.sliced = True
         # If adding an assignment, ensure that it is declared by adding lvalue
         # to dependency list
+        # FIXME: Only need to declare this variable, not handle all its dependences
         if isinstance(node, c_ast.Assignment):
-          self.id_visitor.visit(node.lvalue)
-          self.rvalues.union(set(self.id_visitor.IDs))
+          self.id_visitor.new_visit(node.lvalue)
+          self.rvalues += self.id_visitor.IDs
         """
         print "Adding to slice:"
         print_node(node)
@@ -147,32 +176,30 @@ class DataDependencyVisitor(c_ast.NodeVisitor):
     # Include condition part of for loop (init and next should be captured as
     # data assignment/unaryops)
     if node.sliced:
-      # self.id_visitor.visit(node.init)
-      # self.rvalues = self.rvalues.union(set(self.id_visitor.IDs))
-      self.id_visitor.visit(node.cond)
-      self.rvalues = self.rvalues.union(set(self.id_visitor.IDs))
-      # self.id_visitor.visit(node.next)
-      # self.rvalues = self.rvalues.union(set(self.id_visitor.IDs))
+      self.id_visitor.new_visit(node.cond)
+      self.rvalues += self.id_visitor.IDs
   def visit_If(self, node):
     self.generic_visit(node)
     # Include if condition
     if node.sliced:
-      self.id_visitor.visit(node.cond)
-      self.rvalues = self.rvalues.union(set(self.id_visitor.IDs))
+      self.id_visitor.new_visit(node.cond)
+      self.rvalues += self.id_visitor.IDs
+      # Continue slicing in if block
       self.generic_visit(node)
   def visit_While(self, node):
     self.generic_visit(node)
     if node.sliced:
       # Include loop condition
-      self.id_visitor.visit(node.cond)
-      self.rvalues = self.rvalues.union(set(self.id_visitor.IDs))
+      self.id_visitor.new_visit(node.cond)
+      self.rvalues += self.id_visitor.IDs
       self.generic_visit(node)
   def visit_DoWhile(self, node):
     self.generic_visit(node)
     # Include loop condition
     if node.sliced:
-      self.id_visitor.visit(node.cond)
-      self.rvalues = self.rvalues.union(set(self.id_visitor.IDs))
+      self.id_visitor.new_visit(node.cond)
+      self.rvalues += self.id_visitor.IDs
+
       self.generic_visit(node)
 
 """
@@ -196,7 +223,7 @@ class PrintSliceVisitor(c_generator.CGenerator):
     if add_indent: self.indent_level += 2
     indent = self._make_indent()
     if add_indent: self.indent_level -= 2
-    
+
     if typ in (
         c_ast.Decl, c_ast.Assignment, c_ast.Cast, c_ast.UnaryOp,
         c_ast.BinaryOp, c_ast.TernaryOp, c_ast.FuncCall, c_ast.ArrayRef,
@@ -233,28 +260,29 @@ def slice_ast(ast, var):
   v = DataDependencyVisitor(var)
   v.visit(ast)
 
-  # Iteratively find all data dependencies
-  rvalues = v.rvalues
-  new_rvalues = rvalues
-  changed = True
-  # Keep propagating until no changes to data dependency list 
-  while changed:
-    changed = False
-    # For each rvalue, find all dependencies for it
+  # Variables already sliced
+  handled_rvalues = [var]
+  # Get list of dependent variables that need to be further sliced
+  rvalues = []
+  for rvalue in v.rvalues:
+    if rvalue not in handled_rvalues and rvalue not in rvalues:
+      rvalues.append(rvalue)
+  # Iteratively find all data dependencies until no new variables to slice
+  while rvalues:
+    new_rvalues = []
+    # For each rvalue
     for rvalue in rvalues:
-      v = DataDependencyVisitor(rvalue)
-      v.visit(ast)
-      new_rvalues = new_rvalues.union(v.rvalues)
-    # If data dependency list has increased, keep looping
-    if len(new_rvalues) > len(rvalues):
-      changed = True
-    rvalues = new_rvalues
-
-  """
-  print '\n'.join(sorted(rvalues))
-  print 
-  print
-  """
+      # Run DataDependencyVisitor to mark slice
+      v.new_visit(rvalue, ast)
+      # Add rvalue to list of sliced values
+      handled_rvalues.append(rvalue)
+      # Keep track of (possible) new rvalues 
+      new_rvalues += v.rvalues
+    # Get list of new rvalues that need to be handled
+    rvalues = []
+    for new_rvalue in new_rvalues:
+      if new_rvalue not in handled_rvalues and new_rvalue not in rvalues:
+        rvalues.append(new_rvalue)
 
 """
 Pretty print the passed AST/node.
@@ -281,9 +309,8 @@ if __name__ == "__main__":
 
   # Generate AST
   ast = parse_file(filename, use_cpp=True)
-  #ast.show(nodenames=True)
   # Generate slice
   slice_ast(ast, var)
-
+  # Output generated slice
   print_slice(ast)
 
