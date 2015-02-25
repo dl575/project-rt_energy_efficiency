fca5227b2eed549236122a1b27f27be409c15de2
Daniel Lo <dl575@cornell.edu>
2015-02-25 13:45:31 -0500

    Update script that plots predicted times.

--- dvfs_sim/plot_prediction.py
+++ dvfs_sim/plot_prediction.py
@@ -5,7 +5,8 @@ import sys
 from parse_lib import *
 from pylab import *
 
-colors = ["#fdbf6f", "#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c"]
+#colors = ["#fdbf6f", "#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c"]
+colors = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999']
 
 def read_predict_file(filename):
   f = open(filename, 'r')
@@ -22,19 +23,22 @@ else:
   sys.exit()
 
 # Original execution time
-times = parse_execution_times("data/%s.txt" % (benchmark))
+times = parse_execution_times("data/%s/%s1.txt" % (benchmark, benchmark))
 plot(times, 'k', linewidth=2)
+#print ','.join(map(str, times))
 
 # Predicted times
 predict_dir = "predict_times/"
-#policies = ["policy_average", "policy_pid_timeliness", "policy_pid_energy", "policy_data_dependent", "policy_data_dependent2", "policy_data_dependent_oracle"]
-policies = ["policy_pid_timeliness", "policy_data_dependent_oracle", "policy_data_dependent_lp"]
+policies = ["policy_tuned_pid", "policy_data_dependent_oracle", "policy_data_dependent_lp"]
 for (i, policy) in enumerate(policies):
-  filename = "predict_times/%s-%s.txt" % (policy, benchmark)
+  filename = predict_dir + "%s-%s.txt" % (policy, benchmark)
   predict_times = read_predict_file(filename)
-  plot(predict_times, '.-', color=colors[i], linewidth=2)
-legend(["original"] + policies, loc="upper center", ncol=2)
+  #print ','.join(map(str, predict_times))
+  plot(predict_times, '.', color=colors[i], linewidth=1)
+#legend(["original"] + policies, loc="upper center", ncol=2)
+legend(["Execution Time", "PID", "Least-Squares Prediction", "Conservative Prediction"])
 ylim([0, 3*max(times)])
+#ylim([0, 1000])
 
 tight_layout()
 show()
