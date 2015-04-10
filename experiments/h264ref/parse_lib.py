4996f8c974dcf81680066186005a5cab101bf8f2
Daniel Lo <dl575@cornell.edu>
2015-04-10 11:55:58 -0400

    Clean-up of repo.

--- experiments/h264ref/parse_lib.py
+++
@@ -1,1 +0,0 @@
-../../lib/parse_lib.py
--- scripts/ffmpeg_convert.sh
+++
@@ -1,8 +0,0 @@
-if [ -z "$1" ]
-then
-  echo "No input file specified"
-  exit
-fi
-
-ffmpeg -i $1 -benchmark -y -s 1000x1000 -an output.avi > ffmpeg_convert_out
-
