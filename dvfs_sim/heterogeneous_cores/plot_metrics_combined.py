0d53b9073bcf423c5a8be63dc699c545bf67063f
Daniel Lo <dl575@cornell.edu>
2015-07-26 17:20:05 -0400

    Split heterogeneous cores into seperate plots for each deadline.

--- dvfs_sim/heterogeneous_cores/plot_metrics_combined.py
+++ dvfs_sim/heterogeneous_cores/plot_metrics_combined.py
@@ -49,7 +49,7 @@ def plot_em(data, benchmarks, metrics, policies, filename):
   else:
     raise Exception("Unsuported number of policies = %d" % len(policies))
 
-  opts.file_name = filename
+  opts.file_name = filename.split('.')[0] + "_em.pdf"
 
   for metric in  ["energy", "deadline_misses"]:
     if metric == "energy":
@@ -61,7 +61,7 @@ def plot_em(data, benchmarks, metrics, policies, filename):
     opts.data = []
     for policy in policies:
       opts.data.append(metric_data[policy])
-      opts.labels[1].append(policy_to_label(policy, False))
+      opts.labels[1].append(policy_to_label(policy))
     opts.data = numpy.array(opts.data).T
 
     # Use one legend for both plots
@@ -88,7 +88,7 @@ def plot_core_counts(data, benchmarks, metrics, policies, filename):
   else:
     raise Exception("Unsuported number of policies = %d" % len(policies))
 
-  opts.file_name = filename
+  opts.file_name = filename.split('.')[0] + "_counts.pdf"
 
   for metric in  ["switch_count", "big_count"]:
     if metric == "switch_count":
@@ -101,7 +101,7 @@ def plot_core_counts(data, benchmarks, metrics, policies, filename):
     for policy in policies:
       if "biglittle" in policy:
         opts.data.append(metric_data[policy])
-        opts.labels[1].append(policy_to_label(policy, True))
+        opts.labels[1].append(policy_to_label(policy))
     opts.data = numpy.array(opts.data).T
 
     # Use one legend for both plots
@@ -112,23 +112,19 @@ def plot_core_counts(data, benchmarks, metrics, policies, filename):
 
     tsg_plot.add_plot(opts)
  
-def policy_to_label(policy, budget):
+def policy_to_label(policy):
   """
   Convert policy function/configuration name to a more human readable format.
   """
-  # Include budget in label
-  if budget:
-    policy = policy.strip().split('-')[1:]
-    policy[-1] = "%.1f" % (float(policy[-1])/100)
-    return '-'.join(policy)
-  else:
-    return policy.strip().split('-')[1]
+  policy = policy.strip().split('-')[1:]
+  policy[-1] = "%.1f" % (float(policy[-1])/100)
+  return '-'.join(policy)
 
 def parse(filename):
   """
   Parse raw data file.
   """
-  data = {"metadata" : {"metrics" : [], "policies" : [], "benchmarks" : [], "deadlines" : []}}
+  data = {"metadata" : {"metrics" : [], "policies" : [], "benchmarks" : []}}
 
   f = open(filename, 'r')
   current_metric = None
@@ -154,11 +150,7 @@ def parse(filename):
       # Store policies on first metric
       if "policy_cvx_conservative_lasso" in policy and len(data["metadata"]["metrics"]) == 1:
         data["metadata"]["policies"].append(policy)
-        data["metadata"]["deadlines"].append(policy.split('-')[-1])
   f.close()
-
-  # Remove redundant entries
-  data["metadata"]["deadlines"] = list(set(data["metadata"]["deadlines"]))
 
   return data
 
@@ -173,38 +165,10 @@ if __name__ == "__main__":
   data = parse(filename)
 
   # Reformat for plotting
+  plot_data = {}
   benchmarks = [x.split('_')[0] for x in data["metadata"]["benchmarks"]]
   policies = data["metadata"]["policies"]
   metrics = data["metadata"]["metrics"]
-  deadlines = data["metadata"]["deadlines"]
-
-  # Seperate energy/miss plots for each deadline
-  for deadline in deadlines:
-    plot_data = {}
-    policies_deadline = []
-    for policy in policies:
-      if policy.split('-')[-1] == deadline:
-        policies_deadline.append(policy)
-
-    for metric, metric_data in data.iteritems():
-      if metric == "metadata":
-        continue
-      # Normalize energy numbers to WC
-      elif metric == "energy":
-        for policy in policies_deadline:
-          if metric not in plot_data.keys():
-            plot_data[metric] = {}
-          plot_data[metric][policy] = metric_data[policy]
-      # Otherwise, just copy data
-      else:
-        plot_data[metric] = {}
-        for policy in policies_deadline:
-          plot_data[metric][policy] = metric_data[policy]
-
-    plot_em(plot_data, benchmarks, metrics, policies_deadline, "%s_%s_em.pdf" % (filename.split('.')[0], deadline))
-
-  # Combined plot for core counts
-  plot_data = {}
   for metric, metric_data in data.iteritems():
     if metric == "metadata":
       continue
@@ -220,4 +184,5 @@ if __name__ == "__main__":
       for policy in policies:
         plot_data[metric][policy] = metric_data[policy]
 
-  plot_core_counts(plot_data, benchmarks, metrics, policies, "%s_counts.pdf" % (filename.split('.')[0]))
+  plot_em(plot_data, benchmarks, metrics, policies, filename)
+  plot_core_counts(plot_data, benchmarks, metrics, policies, filename)
