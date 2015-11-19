9b140848c8aae282dcd1d056e7b9b89555f630e4
TJSong <ts693@cornell.edu>
2015-11-19 16:43:03 +0000

    fix bug in run.sh (kill cc process)

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -21,7 +21,7 @@ if [ $2 == "big" ] ; then
     WHICH_CPU="cpu7"
     TASKSET_FLAG="0x80"
     MAX_FREQ=2000000
-    SENSOR_ID="3-0040"
+    SENSOR_ID="2-0040"
 elif [ $2 == "little" ] ; then
 	if [ $ARCH_TYPE == "amd64" ] ; then 
     WHICH_CPU="cpu3"
@@ -35,12 +35,12 @@ elif [ $2 == "little" ] ; then
 		echo "unknown architecture"
 		exit 1
 	fi
-  SENSOR_ID="3-0045"
+  SENSOR_ID="2-0045"
 elif [ $2 == "hetero" ] ; then
   WHICH_CPU="cpu3"
   TASKSET_FLAG="0x08"
   MAX_FREQ=1400000
-  SENSOR_ID="3-0045"
+  SENSOR_ID="2-0045"
 fi
 
 init(){
@@ -53,13 +53,15 @@ init(){
   elif [ $ARCH_TYPE == "armhf" ] ; then 
     sudo chmod 777 /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
     sudo chmod 777 /sys/devices/system/cpu/cpu7/cpufreq/scaling_max_freq
-    sudo chmod 777 /sys/bus/i2c/drivers/INA231/3-0040/sensor_W
+    echo 1 > /sys/bus/i2c/drivers/INA231/2-0040/enable
+    sudo chmod 777 /sys/bus/i2c/drivers/INA231/2-0040/sensor_W
     sudo chmod 777 /sys/devices/system/cpu/cpu7/cpufreq/scaling_cur_freq
     echo 2000000 > /sys/devices/system/cpu/cpu7/cpufreq/scaling_max_freq 
     echo performance > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
     sudo chmod 777 /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
     sudo chmod 777 /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-    sudo chmod 777 /sys/bus/i2c/drivers/INA231/3-0045/sensor_W
+    echo 1 > /sys/bus/i2c/drivers/INA231/2-0045/enable
+    sudo chmod 777 /sys/bus/i2c/drivers/INA231/2-0045/sensor_W
     sudo chmod 777 /sys/devices/system/cpu/cpu3/cpufreq/scaling_cur_freq
     echo 1400000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq 
     echo performance > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
@@ -237,6 +239,8 @@ if [[ $3 ]] ; then
       cp $PRE_PWD/output_slice.txt $PROJECT_PATH/dvfs_sim/data_odroid/$2/$BENCHMARK_FOLDER/$BENCHMARK/$3
       PID_DUMMY=$(pgrep 'dummy')
       sudo kill -9 $PID_DUMMY
+      PID_CC=$(pgrep 'cc')
+      sudo kill -9 $PID_CC
     ) 200>/var/lock/lockfile
 else
     echo "specify governor!"
